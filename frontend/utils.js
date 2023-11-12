dea1dda0b068c9e9e887e207e8cc91fa5a9cabf9
labarta <93525195+tlabarta@users.noreply.github.com>
2023-11-12 23:32:38 +0100

    Selecting parts of the SVG is finally working!

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,42 +1,111 @@
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const socket = io();
+
+        // Get the selected XAI method from the dropdown
+    var xaiMethodSelector = document.getElementById('xaiMethodDropdown');
+
+    var selectedXaiMethod = xaiMethodSelector.value;
+
+    // Set explanation text to default
+    updateXaiInfoText(xaiMethodSelector.value);
+    document.getElementById('currentXAIselection').textContent = "XAI Method: " + xaiMethodSelector.value;
+    document.getElementById('selectedNeuron').textContent = "Selected Neuron: 19";
+
+
+    // Emit 'videoframe' event with dataset ID and selected XAI method
+    socket.emit('select_input', {xai_method: selectedXaiMethod });
+    socket.emit('explain_prediction', {xai_method: xaiMethodSelector.value });
+
+           // Add an event listener for the change event on the dropdown
+    xaiMethodSelector.addEventListener('change', function () {
+        // Emit 'explain_prediction' event with updated XAI method
+        updateXaiInfoText(xaiMethodSelector.value);
+        document.getElementById('currentXAIselection').textContent = "XAI Method: " + xaiMethodSelector.value;
+        document.getElementById('selectedNeuron').textContent = "Selected Neuron: 19";
+        socket.emit('explain_prediction', {xai_method: xaiMethodSelector.value });
+
+    });
+
+    document.getElementById('action_button').addEventListener('click', function () {
+    // Emit a custom event to the server
+        socket.emit('button_click', {xai_method: xaiMethodSelector.value });
+    });
+
+    // Listen for the 'input_image' event from the server
+    socket.on('input_image', function (data) {
+        // Update the src attribute of the img element with the received image path
+
+        document.getElementById('input_img').src = data.data + '?' + new Date().getTime();
+
+    });
+
+    // Listen for the 'prediction' event from the server
+    socket.on("prediction", function (data) {
+        var prediction = data.prediction;
+        var actual_label = data.actual_label;
+        console.log(prediction);
+
+        const responseText = document.getElementById('prediction');
+        responseText.textContent = 'Predicted number: ' + String(prediction);
+
+        const actLabel = document.getElementById('label');
+        actLabel.textContent = 'Actual number: ' + String(actual_label);
+
+    });
+
+
+    socket.on("heatmap", function (data) {
+        var heatmapResponse = data.data;
+        console.log(heatmapResponse)
+        document.getElementById('explanation_img').src = heatmapResponse + '?' + new Date().getTime();
+    });
+
+    const nodes = document.querySelectorAll('.node'); // Select nodes with the class 'node'
+    let selectedNode = null; // To keep track of the currently selected node
+
+    // Add a click event listener to each node
+    nodes.forEach(node => {
+        node.addEventListener('click', function() {
+            // If another node was previously selected, reset its color
+            if (selectedNode !== null) {
+                selectedNode.style.fill = 'rgb(255, 255, 255)'; // Reset color to default (empty string)
+            }
+
+            // Log the node ID to the console
+            console.log('Clicked Node ID:', this.id);
+            console.log('Successful');
+
+            // Change the fill color to red
+            this.style.fill = 'red';
+
+            // Update the selectedNode variable
+            selectedNode = this;
+        });
+    });
 
 
 
+});
 
 
+    // Function to update xai_info text based on the selected value
+function updateXaiInfoText(selectedXaiMethod) {
+    // Hide all xai-info-text elements
+    document.querySelectorAll('.xai-info-text').forEach(function(element) {
+        element.style.display = 'none';
+    });
 
-
-
-function encode(input) {
-    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    var output = "";
-    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
-
-    while (i < input.length) {
-        chr1 = input[i++];
-        chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index 
-        chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
-        }
-        output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-            keyStr.charAt(enc3) + keyStr.charAt(enc4);
+    // Show the relevant xai-info-text based on the selected XAI method
+    switch (selectedXaiMethod) {
+        case 'lrp':
+            document.getElementById('lrpInfo').style.display = 'block';
+            break;
+        case 'crp':
+            document.getElementById('crpInfo').style.display = 'block';
+            break;
+        // Add more cases as needed for other XAI methods
+        // ...
     }
-    return output;
 }
-
-String.prototype.capitalize = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
-
-
